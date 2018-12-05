c99a906326be227055e8bb3a64b8862abb04c26c
Robert <rsheldiii@gmail.com>
2018-12-05 15:22:32 -0500

    optimistically update to new header auth

--- lib/pardot/http.rb
+++ lib/pardot/http.rb
@@ -1,63 +1,69 @@
 module Pardot
   module Http
-    
+
     def get object, path, params = {}, num_retries = 0
       smooth_params object, params
       full_path = fullpath object, path
-      check_response self.class.get(full_path, :query => params)
-      
+      check_response self.class.get(full_path, :query => params, :headers => auth_headers(object))
+
     rescue Pardot::ExpiredApiKeyError => e
       handle_expired_api_key :get, object, path, params, num_retries, e
-      
+
     rescue SocketError, Interrupt, EOFError, SystemCallError, Timeout::Error, MultiXml::ParseError => e
       raise Pardot::NetError.new(e)
     end
-    
+
     def post object, path, params = {}, num_retries = 0
       smooth_params object, params
       full_path = fullpath object, path
       check_response self.class.post(full_path, :query => params)
-      
+
     rescue Pardot::ExpiredApiKeyError => e
       handle_expired_api_key :post, object, path, params, num_retries, e
-      
+
     rescue SocketError, Interrupt, EOFError, SystemCallError, Timeout::Error, MultiXml::ParseError => e
       raise Pardot::NetError.new(e)
     end
-    
+
     protected
-    
+
     def handle_expired_api_key method, object, path, params, num_retries, e
       raise e unless num_retries == 0
-      
+
       reauthenticate
-      
+
       send(method, object, path, params, 1)
     end
-    
+
     def smooth_params object, params
       return if object == "login"
-      
-      authenticate unless authenticated?
-      params.merge! :user_key => @user_key, :api_key => @api_key, :format => @format
+
+      params.merge! :format => @format
     end
-    
+
+    def auth_headers(object)
+      return if object == "login"
+
+      authenticate unless authenticated?
+      { "Authorization" => "Pardot api_key=#{@api_key}, user_key=#{@user_key}" }
+    end
+
     def check_response http_response
       rsp = http_response["rsp"]
-      
+
       error = rsp["err"] if rsp
       error ||= "Unknown Failure: #{rsp.inspect}" if rsp && rsp["stat"] == "fail"
       content = error['__content__'] if error.is_a?(Hash)
-      
+
       if [error, content].include?("Invalid API key or user key") && @api_key
         raise ExpiredApiKeyError.new @api_key
       end
-      
+
       raise ResponseError.new error if error
-      
+
       rsp
     end
-    
+
     def fullpath object, path
       full = File.join("/api", object, "version", @version.to_s)
       unless path.nil?
@@ -65,6 +71,6 @@ module Pardot
       end
       full
     end
-    
+
   end
 end
